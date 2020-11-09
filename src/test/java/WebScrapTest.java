63aa4ae7b2227927dbd1fe2ba5e4c9f74013530c
Monil Joshi <monil.joshi@qualitia.local>
2020-11-09 11:25:24 +0530

    MongoDB with Selenium for WebScrapping | Add one more website Snapdeal for web scrapping

--- src/test/java/WebScrapTest.java
+++ src/test/java/WebScrapTest.java
@@ -6,6 +6,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.bson.Document;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.*;
@@ -38,7 +39,8 @@ public class WebScrapTest {
     public Object[][] getWebData(){
         return new Object[][]{
                 {"https://www.amazon.com/"},
-                {"https://www.flipkart.com/"}
+                {"https://www.flipkart.com/"},
+                {"https://www.snapdeal.com/"}
         };
     }
 
